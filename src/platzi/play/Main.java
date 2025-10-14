cd25534c3a4fb9f565b3fbfab2901ef8a95726c4
cesaranayaplai <cesar.anaya@plai.mx>
2025-10-14 12:01:10 -0600

    Emoji palomitas

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
-    public static final String NOMBRE_PLATAFORMA = "PLATZI PLAY ðŸ¿";
+    public static final String NOMBRE_PLATAFORMA = "PLATZI PLAY 🍿";
     public static final String VERSION = "1.0.0";
 
     public static void main(String[] args) {
